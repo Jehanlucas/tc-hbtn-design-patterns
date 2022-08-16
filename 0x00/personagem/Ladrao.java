47ff47a6ddb1e44200ff7360f0ee106d24df7005
Jehan Lucas Vieira e Silva <jehan@ciandt.com>
2022-08-16 13:37:04 -0300

    Personagem RPG

---
+++ 0x00/personagem/Ladrao.java
@@ -0,0 +1,14 @@
+public class Ladrao extends Personagem{
+
+    public Ladrao(String nome,  int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+        super(nome, TipoPersonagem.LADRAO, inteligencia, forca, vigor, resistencia, destreza);
+        if (forca > destreza && inteligencia > destreza){
+            throw new IllegalArgumentException("Atributos invalidos para LADRÃO");
+        }
+    }
+
+    @Override
+    public double getDanoAtaque() {
+        return (forca * 0.5) + (destreza * 0.35) + (vigor * 0.1) + (inteligencia* 0.05);
+    }
+}
